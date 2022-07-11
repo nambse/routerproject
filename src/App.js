dba444c65ed6f03307109f1325db46b19d7d73a2
nambse <sefa.esendemir@gmail.com>
2022-07-12 02:12:12 +0300

    prompt

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+import NotFound from './pages/NotFound';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Switch>
-        <Route path="/quotes">
-
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
         </Route>
-        <Route path="/quotes/:quoteId">
-          
+        <Route path='/quotes' exact>
+          <AllQuotes />
         </Route>
-        <Route path="/new-quote">
-          
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
